961e28ad9686a7a085e3789ce1307a5769190040
Sachin V S <sachinsvs13@gmail.com>
2026-02-10 20:45:29 +0530

    simple Backend

---
+++ Back-end/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketIo(server);
+const PORT = process.env.PORT || 3000;
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("Message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
